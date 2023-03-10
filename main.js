42b49933f349dfdcc44732d4b2f2f15f237857a7
HarukaDoo <buriakkdiana@gmail.com>
2023-03-10 16:28:40 +0100

    vinyl history category add

--- main.js
+++ main.js
@@ -32,3 +32,35 @@ const dragStop = () => {
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mouseup", dragStop);
+
+
+
+//прокрутка вниз за допомогою кнопок
+function scrollTo(element) {
+    window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+var contactButton = document.querySelector('.contact__button');
+var contactBlock = document.querySelector('.footer');
+
+contactButton.addEventListener('click', () => {
+    scrollTo(contactBlock);
+})
+
+var assortmentButton = document.querySelector('.assortment__button');
+var assortmentBlock = document.querySelector('.genre__container');
+
+assortmentButton.addEventListener('click', () => {
+    scrollTo(assortmentBlock);
+})
+
+var aboutButton = document.querySelector('.about__button');
+var aboutBlock = document.querySelector('.history-article__container');
+
+aboutButton.addEventListener('click', () => {
+    scrollTo(aboutBlock);
+})
